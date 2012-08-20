12dc06ca14a3feabe115ce1b70cbec956fc548dd
rene.kervezee@gmail.com <rene.kervezee@gmail.com@3f736954-6044-2196-c51c-722df2d0b388>
2012-08-20 08:15:05 +0000

    this is a new dashboard invoices to receive. it shows all invoices that have the check-box wait for invoice checked.

---
+++ enhancements/Dashboards/to_receive_inv/language/en_us/language.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------+
+// |                   PhreeBooks Open Source ERP                    |
+// +-----------------------------------------------------------------+
+// | Copyright (c) 2008, 2009, 2010, 2011, 2012 PhreeSoft, LLC       |
+// | http://www.PhreeSoft.com                                        |
+// +-----------------------------------------------------------------+
+// | This program is free software: you can redistribute it and/or   |
+// | modify it under the terms of the GNU General Public License as  |
+// | published by the Free Software Foundation, either version 3 of  |
+// | the License, or any later version.                              |
+// |                                                                 |
+// | This program is distributed in the hope that it will be useful, |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of  |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   |
+// | GNU General Public License for more details.                    |
+// +-----------------------------------------------------------------+
+//  Path: /modules/phreebooks/dashboards/to_receive_inv/language/en_us/language.php
+//
+
+define('CP_TO_RECEIVE_INV_TITLE','Invoices To Receive');
+define('CP_TO_RECEIVE_INV_DESCRIPTION','Lists invoices that are marked waiting for invoice. Links to reveiw the invoice are also provided.');
+define('CP_TO_RECEIVE_INV_SECURITY',SECURITY_ID_PURCHASE_INVENTORY);
+
+define('CP_TO_RECEIVE_INV_NO_RESULTS','No results found!');
+
+?>

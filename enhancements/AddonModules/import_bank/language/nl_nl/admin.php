6b8eae8e277e1c9f66585a9d4d7fef8b26e676d4
rene.kervezee@gmail.com <rene.kervezee@gmail.com@3f736954-6044-2196-c51c-722df2d0b388>
2013-01-15 14:45:29 +0000

    this is a update in the import_bank module.

---
+++ enhancements/AddonModules/import_bank/language/nl_nl/admin.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------+
+// Phreedom Taal Vertaal Bestand
+// Generated: 2011-04-30 04:23:26
+// +-----------------------------------------------------------------+
+// Path: /modules/import_bank/language/nl_nl/admin.php
+
+define('MODULE_IMPORT_BANK_TITLE','Importeer Bank rekening');
+define('MODULE_IMPORT_BANK_DESCRIPTION','met deze module kunt u uw .csv bank rekeningen overzicht importeren deze wordt dan gecontroleerd op openstaande rekening of eventueel eerdere boekingen met dezelfde omschrijving.');
+define('BOX_BANK_IMPORT_ADMIN','Bank Import Instellingen');
+define('TEXT_BANK_IMPORT_SETTINGS','Bank Import Instellingen');
+define('MODULE_BANK_IMPORT_CONFIG_INFO','Stel alstublieft uw configuratie waarde in');
+define('BANK_IMPORT_QUESTION_POSTS','Om te voorkomen dat boekingen niet geboekt worden wordt door het script wordt als laatste optie de boekingen opgeslagen op de grootboek vraag posten. <B>Dit moet ingevuld worden anders werkt het script niet</B>');
+define('BANK_IMPORT_DEBIT_CREDIT','De omschrijving als er bedragen op uw rekening bijgeschreven zijn. <br><B>dit is voor het xml veld debit_credit</B>');
+define('IMPORT_BANK_CONFIG_SAVED','saved import_bank config');
+//1.1
+define('IMPORT_BANK_CAN_NOT_DECREASE_NUMBER_OF_BANK_ACCOUNTS','Kan het aantal bankrekeningvelden niet verkleinen');
+define('BANK_IMPORT_NUMBER_OF_BANK_ACCOUNTS','aantal bankrekeningvelden <b>kan niet minder dan het huidige aantal.</b>');
+?>

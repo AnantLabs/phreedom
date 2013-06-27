a70a10bb536749f994b2a4c5472b4c03c7a294a1
rene.kervezee@gmail.com <rene.kervezee@gmail.com@3f736954-6044-2196-c51c-722df2d0b388>
2013-06-27 18:27:03 +0000

    is in current release

--- branches/feature/InventoryClasses/inventory/classes/type/sf.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(DIR_FS_MODULES . 'inventory/classes/inventory.php');
-class sf extends inventory {//Flat Rate - Service
-	public $title			       	= INV_TYPES_SF;
-	public $account_sales_income	= INV_SERVICE_DEFAULT_SALES;
-	public $account_inventory_wage	= INV_SERVICE_DEFAULT_INVENTORY;
-	public $account_cost_of_sales	= INV_SERVICE_DEFAULT_COS; 
-}
--- branches/feature/InventoryClasses/inventory/classes/type/sv.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(DIR_FS_MODULES . 'inventory/classes/inventory.php');
-class sv extends inventory {//Service
-	public $title       			= INV_TYPES_SV;
-	public $account_sales_income	= INV_SERVICE_DEFAULT_SALES;
-	public $account_inventory_wage	= INV_SERVICE_DEFAULT_INVENTORY;
-	public $account_cost_of_sales	= INV_SERVICE_DEFAULT_COS; 
-		
-}
